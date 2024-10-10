01cb8d4a2ee07ed8fdae99abe3a6017b032ca527
asma-abichou <abichou.asmaa@gmail.com>
2024-10-10 15:26:54 +0100

    create pagination for table ingredient with bundel knp paginator

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,17 +3,25 @@
 namespace App\Controller;
 
 use App\Repository\IngredientRepository;
+
+
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IngredientController extends AbstractController
 {
     #[Route('/ingredient', name: 'app_ingredient')]
-    public function index(IngredientRepository $ingredient): Response
+    public function index(IngredientRepository $ingredient ,PaginatorInterface $paginator, Request $request): Response
     {
-        $ingredient = $ingredient->findAll();
-       // dd($ingredientRepository);
+        $ingredient = $paginator->paginate(
+            $ingredient->findAll(),
+            $request->query->getInt('page', 1), /* page number */
+            10 /* limit per page */
+        );
+
         return $this->render('pages/ingredient/index.html.twig',[
             'ingredients' => $ingredient
         ]);
